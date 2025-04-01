d2863b9dd312a90a6ab9b9319028ef90692c4ac5
InczeZ <z.incze2003@gmail.com>
2025-04-01 20:55:39 +0300

    lab2_1

--- lab1/DatesAndStuff/src/DatesAndStuff/Person.cs
+++ lab1/DatesAndStuff/src/DatesAndStuff/Person.cs
@@ -35,6 +35,13 @@ namespace DatesAndStuff
 
         public const double SubscriptionFee = 500;
 
+        public Person(string name, double salary)
+        :this(name, new EmploymentInformation(salary, null), null, null, new FoodPreferenceParams())
+        {
+
+
+        }
+
         public Person(string name, EmploymentInformation employment, IPaymentService paymentService, LocalTaxData taxData, FoodPreferenceParams foodPreferenceParams)
         {
             this.Name = name;
